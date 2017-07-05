2bd5e0247cc7a895870f6c31b64952ea64406354
Angela <angeladon22@gmail.com>
2017-07-05 14:20:22 -0400

    hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
  def hello
-     render text: "<h1>hello</h1><p>Welcome</p>"
+     render text: "<h1>Hello</h1><p>Welcome home</p>"
    end
 end
